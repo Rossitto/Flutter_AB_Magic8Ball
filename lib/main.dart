98785f4e07e7f77530007c3a4d660cebbe6643e5
Lucas Schroeder Rossi <rossittao@MacBook-Pro.local>
2020-04-21 13:59:45 -0300

    image ball1 displayed centered!

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,28 @@ class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue,
       appBar: AppBar(
         backgroundColor: Colors.blue.shade900,
         title: Text('Ask me anything!'),
       ),
-      body: Container(),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Image.asset('images/ball1.png'),
+      ),
     );
   }
 }
